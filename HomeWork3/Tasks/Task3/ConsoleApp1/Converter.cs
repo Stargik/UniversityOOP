0d0a7c3688cc0b403fa777c5e24edbd429f8bdb8
Stargik <a.stargik@gmail.com>
2022-09-25 18:03:03 +0300

    Aded Task3

---
+++ HomeWork3/Tasks/Task3/ConsoleApp1/Converter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Converter
+    {
+        private readonly decimal currencyUSDtoUAH;
+        private readonly decimal currencyEURtoUAH;
+
+        public Converter(decimal currencyUSDtoUAH, decimal currencyEURtoUAH)
+        {
+            this.currencyUSDtoUAH = currencyUSDtoUAH;
+            this.currencyEURtoUAH = currencyEURtoUAH;
+        }
+        public decimal Convert(decimal value, string from, string to)
+        {
+            if (from == "UAH")
+            {
+                switch (to)
+                {
+                    case "USD":
+                        if (currencyUSDtoUAH == 0)
+                        {
+                            throw new ArgumentException("currencyUSDtoUAH can't be null");
+                        }
+                        return value / currencyUSDtoUAH;
+                    case "EUR":
+                        if (currencyEURtoUAH == 0)
+                        {
+                            throw new ArgumentException("currencyEURtoUAH can't be null");
+                        }
+                        return value / currencyEURtoUAH;
+                    default:
+                        throw new ArgumentException("Incorrect currency");
+                }
+            }
+            else if (from == "USD")
+            {
+                return value * currencyUSDtoUAH;
+            }
+            else if (from == "EUR")
+            {
+                return value * currencyEURtoUAH;
+            }
+            else
+            {
+                throw new ArgumentException("Incorrect currency");
+            }
+        }
+    }
+}
---
+++ HomeWork3/Tasks/Task3/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter;
+            decimal usdCurrency, eurCurrency;
+            while (true)
+            {
+                Console.WriteLine("Enter usd currency: ");
+                string? usd = Console.ReadLine();
+                Console.WriteLine("Enter eur currency: ");
+                string? eur = Console.ReadLine();
+                if (Decimal.TryParse(usd, out usdCurrency) && Decimal.TryParse(eur, out eurCurrency))
+                {
+                    if (usdCurrency != 0 && eurCurrency != 0)
+                    {
+                        converter = new Converter(usdCurrency, eurCurrency);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect input");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input");
+                }
+            }
+            while (true)
+            {
+                decimal value;
+                Console.WriteLine("Enter value(f - for finish): ");
+                string? input = Console.ReadLine();
+                if (input == "f")
+                {
+                    break;
+                }
+                Console.WriteLine("Enter current currency (USD/EUR/UAH): ");
+                string? current = Console.ReadLine();
+                Console.WriteLine("Enter convert currency (USD/EUR for current UAH or UAH for current USD/EUR): ");
+                string? convert = Console.ReadLine();
+                if (Decimal.TryParse(input, out value) && !String.IsNullOrEmpty(current) && !String.IsNullOrEmpty(convert))
+                {
+                    try
+                    {
+                        decimal res = converter.Convert(value, current, convert);
+                        Console.WriteLine($"Result: {res}");
+                    }
+                    catch (Exception)
+                    {
+
+                        Console.WriteLine("Incorrect input");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input");
+                }
+            }
+
+
+        }
+    }
+}
