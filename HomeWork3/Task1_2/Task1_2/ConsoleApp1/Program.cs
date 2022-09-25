4e4f665befeade86fc7027bad9b5d3ec265137a9
Stargik <a.stargik@gmail.com>
2022-09-25 14:50:30 +0300

    Added ConsolApp

---
+++ HomeWork3/Task1_2/Task1_2/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            string? mes = Console.ReadLine();
+            int number;
+            if (int.TryParse(mes, out number))
+            {
+                Console.WriteLine($"Ви ввели число {number}");
+            }
+            else
+            {
+                Console.WriteLine("Ви ввели не число");
+            }
+        }
+    }
+}
