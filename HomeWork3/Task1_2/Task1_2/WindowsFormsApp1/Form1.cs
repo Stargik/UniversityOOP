7237ded02df7d19a298361bbeeefd40e8dda8766
Stargik <a.stargik@gmail.com>
2022-09-25 15:19:38 +0300

    Added winforms app

---
+++ HomeWork3/Task1_2/Task1_2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox1.Text))
+            {
+                lblNumber.Text = $"Ви ввели число {textBox1.Text}";
+                lblNumber.Visible = true;
+                textBox1.Text = null;
+            }
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && !((sender as TextBox).Text.Length == 0 && (e.KeyChar == '-' || e.KeyChar == '+')))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
