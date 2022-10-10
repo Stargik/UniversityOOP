d06fc99857fb7eb01ba84b52eed0d054bbad234a
Stargik <a.stargik@gmail.com>
2022-10-10 17:55:14 +0300

    Added polymorphism with override/new

---
+++ HomeWork4/Polymorphism/Polymorphism1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Polymorphism1
+{
+    public class Class1
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Class1 Method1");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("Class1 Method2");
+        }
+    }
+
+    public class Class2 : Class1
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Class2 Method1");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("Class2 Method2");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 x = new Class1();
+            Class2 y = new Class2();
+            Class1 z = new Class2();
+
+            x.Method1();
+            x.Method2();
+            Console.WriteLine("");
+            y.Method1();
+            y.Method2();
+            Console.WriteLine("");
+            z.Method1();
+            z.Method2();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork4/Polymorphism/Polymorphism3/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Polymorphism3
+{
+    public class Class1
+    {
+        public virtual void Method1()
+        {
+            Console.WriteLine("Class1 Method1");
+        }
+        public virtual void Method2()
+        {
+            Console.WriteLine("Class1 Method2");
+        }
+        public virtual void Method3()
+        {
+            Console.WriteLine("Class1 Method3");
+        }
+    }
+
+    public class Class2 : Class1
+    {
+        public new void Method1()
+        {
+            Console.WriteLine("Class2 Method1");
+        }
+        public override void Method2()
+        {
+            Console.WriteLine("Class2 Method2");
+        }
+        public void Method3()
+        {
+            Console.WriteLine("Class2 Method2");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 x = new Class1();
+            Class2 y = new Class2();
+            Class1 z = new Class2();
+
+            x.Method1();
+            x.Method2();
+            x.Method3();
+            Console.WriteLine("");
+            y.Method1();
+            y.Method2();
+            y.Method3();
+            Console.WriteLine("");
+            z.Method1();
+            z.Method2();
+            z.Method3();
+            Console.ReadKey();
+        }
+    }
+}
